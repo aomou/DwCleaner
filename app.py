465233511d5bad8176528a3a63461768b0469cb2
Hsi Chen <aomou@users.noreply.huggingface.co>
2024-12-05 17:27:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import math
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+a = st.number_input('Input an area (m)')
+r = math.sqrt(a / math.pi)
+d = 2 * r
+l = math.sqrt(a)
+
+st.write('A circle with an area of', a, 'has a radius of', r, 'and a diameter of', d)
+st.write('A square with an area of', a, 'has a side length of', l)
