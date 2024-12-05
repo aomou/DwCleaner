9e61d11dc17d79572c2969ff607c820bfbb1cdf2
aomou <100065388+aomou@users.noreply.github.com>
2024-12-06 01:34:06 +0800

    Add round and units

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import math
 
-a = st.number_input('Input an area (m)')
-r = math.sqrt(a / math.pi)
-d = 2 * r
-l = math.sqrt(a)
+a = st.number_input('Input an area (m2)')
+r = round(math.sqrt(a / math.pi), 2)
+d = round(2 * r, 2)
+l = round(math.sqrt(a), 2)
 
-st.write('A circle with an area of', a, 'has a radius of', r, 'and a diameter of', d)
-st.write('A square with an area of', a, 'has a side length of', l)
+st.write('A circle with an area of', a, 'has a radius of', r, 'meters and a diameter of', d, 'meters.')
+st.write('A square with an area of', a, 'has a side length of', l, 'meters.')
