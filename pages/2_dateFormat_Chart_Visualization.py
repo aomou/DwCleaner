31eaf5f2ac28575d46809a8d34b4ce216b6e3420
Lang <Tina1919@users.noreply.huggingface.co>
2025-01-06 09:13:09 +0000

    Update pages/2_dateFormat_Chart_Visualization.py

--- pages/2_dateFormat_Chart_Visualization.py
+++ pages/2_dateFormat_Chart_Visualization.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-  st.title("圖表可視化")
+    st.title("圖表可視化")
     # 確認第一頁有上傳並處理過資料
     if "df" not in st.session_state or st.session_state.df is None:
         st.warning("尚未上傳或處理資料，請先回到『1_📁_Upload_and_Edit』頁面。")
