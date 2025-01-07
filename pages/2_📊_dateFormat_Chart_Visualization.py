e4363229831c3e19ba23a5a22ed03988660bacc9
Lang <Tina1919@users.noreply.huggingface.co>
2025-01-07 02:52:49 +0000

    Update pages/2_📊_dateFormat_Chart_Visualization.py

--- pages/2_📊_dateFormat_Chart_Visualization.py
+++ pages/2_📊_dateFormat_Chart_Visualization.py
@@ -5,10 +5,12 @@ from datetime import datetime
 import numpy as np
 
 def main():
+    error = False
     st.title("圖表可視化")
+    
     # 確認第一頁有上傳並處理過資料
     if "df" not in st.session_state or st.session_state.df is None:
-        st.warning("尚未上傳或處理資料，請先回到『1_📁_Upload_and_Edit』頁面。")
+        st.warning("尚未上傳或處理資料，請先回到『1_📁_Upload』頁面。")
         return
 
     df = st.session_state.df
