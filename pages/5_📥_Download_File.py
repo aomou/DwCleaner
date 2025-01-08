1a10b89305d1db600c5bbf0d9a3d77cb89b3b1a0
Lang <Tina1919@users.noreply.huggingface.co>
2025-01-08 03:44:49 +0000

    Update pages/5_📥_Download_File.py

--- pages/5_📥_Download_File.py
+++ pages/5_📥_Download_File.py
@@ -20,10 +20,22 @@ def main():
     
     # 提供下載按鈕
     st.download_button(
-        label="下載處理過的資料",
+        label="下載csv",
         data=csv,
         file_name="updated_data.csv",
         mime="text/csv"
     )
+
+    # 將DataFrame 轉換為 Excel 格式
+    xlsx = df.to_xlsx(index=False).encode("utf-8")
+
+    # 提供下載按鈕
+    st.download_button(
+        label="下載xlsx",
+        data=xlsx,
+        file_name="updated_data.xlsx"
+        mime="text/xlsx"
+    )
+    
 if __name__ == "__main__":
     main()
